171af117c1866bdb5cb3c4f2294b9ecb7dc30b72
moreno <moreno>
2021-07-07 20:57:13 +0200

    Scrape more data

--- inject-dzctl.js
+++ inject-dzctl.js
@@ -20,6 +20,8 @@ var injected_dzctl;
 
     function process(command, params) {
         const handlers = {
+
+            // Click on the item with the given selector id
             clickOn: function (selectorId) {
                 const selectors = {
                     togglePlay: '#page_player > div > div.player-controls > ul > li:nth-child(3) > button',
@@ -31,6 +33,8 @@ var injected_dzctl;
                     clickSelector(selector)
                 }
             },
+
+            // Search for the given text
             search: function (text) {
                 console.log("search", text)
 
@@ -46,17 +50,21 @@ var injected_dzctl;
                     }
                 }
 
-                // Trigger search
+                // Write into search box
                 const searchBox = document.querySelector('#topbar-search')
                 if (!searchBox) return
                 setNativeValue(searchBox, text)
                 searchBox.dispatchEvent(new Event('input', { bubbles: true }));
+
+                // Trigger search
                 const form = document.querySelector('#page_topbar > div.topbar-search > div > form')
                 form.requestSubmit()
 
                 // Update status
                 checkStatus()
             },
+
+            // Click on an anchor with the given href
             navigate : function(path){
                 const link = document.querySelector("a[href='"+path+"']");
                 if(!link){
@@ -64,18 +72,120 @@ var injected_dzctl;
                     return
                 }
                 link.click()
-            }
+            },
+
+            // Click on a button with the given selector
+            play : function(playid){
+                const button = document.querySelector(playid);
+                if(!button){
+                    console.log("Could not play "+playid+", no buttons found")
+                    return
+                }
+                button.click()
+            },
+
+            // Scroll an item with the given selector into view
+            scroll : function(selector){
+                const item = document.querySelector(selector);
+                if(!item){
+                    console.log("Could not scroll to "+selector+", no item found")
+                    return
+                }
+                item.scrollIntoView()
+            },
         }
+
         if (handlers[command])
             handlers[command](params)
         else
             console.log("Unknown command", command)
     }
 
-
+    // Transform an object into a plain JSON object
     function tojo(o) {
         return JSON.parse(JSON.stringify(o))
     }
+
+    // Retrieve main navigation items
+    function getMainNavi(){
+        const naviSelector = '#page_sidebar > div.sidebar-nav.nano.has-scrollbar > div.nano-content > div > ul > li > a'
+        const naviLinks = Array.from(document.querySelectorAll(naviSelector))
+        return naviLinks.map(function(link){
+            return {
+                path : link.pathname,
+                html : link.innerHTML
+            }
+        })        
+    }
+
+    // Scrape the list of playlists
+    function getPlaylists() {
+        const selLi = '#page_profile > div.naboo-catalog-content-wrapper > div > div > section > div > ul > li'          
+        const selPlay = 'figure > ul > li:nth-child(1) > button'
+        const selLink = 'div > div.heading-4 > a'
+        const playlists = Array.from(document.querySelectorAll(selLi)).map(function(li){
+            const link = li.querySelector(selLink)
+            if(!link)return
+            const play = li.querySelector(selPlay)
+            // Mark button adding a special classname
+            const playid = link.pathname.replaceAll("/","_")
+            play.classList.add(playid)
+            return {
+                name : link.innerText,
+                path : link.pathname,
+                play : "button."+playid
+            }
+        })
+        return playlists
+    }
+
+    /*
+    function getAllTracks(){
+        // Only visible tracks are loaded on the dom, get them from internal react state
+        const selList = '#page_naboo_playlist > div.catalog-content > div > div:nth-child(2) > div > div.datagrid'
+        const list = document.querySelector(selList)
+        if(!list)return
+        function getReactState(elem){
+            const result = Object.getOwnPropertyNames(elem).filter(function(propName){
+                return propName.match("reactInternalInstance")
+            })
+            if(result.length>0)
+                return elem[result[0]]
+            return null
+        }
+        const listState = getReactState(list)
+        const tracks = tojo(listState.memoizedProps.children[0].props.items)
+
+        return tracks
+    }
+    */
+
+    // Scrape visible tracks
+    function getTracks(){
+        const setPlistItems = '#page_naboo_playlist > div.catalog-content > div > div > div > div.datagrid > div > div.datagrid-row.song'
+        const selAlbumItems = '#page_naboo_album > div > div > div.datagrid-container > div.datagrid > div.datagrid-row.song'
+        const selTitle = 'div.datagrid-cell.cell-title > div > a'
+        const selPlay  = 'div:nth-child(1) > div > a'
+        var items = document.querySelectorAll(setPlistItems)
+        if(items.length==0)
+            items = document.querySelectorAll(selAlbumItems)
+        const tracks = Array.from(items).map(function(item){
+            const title = item.querySelector(selTitle)
+            const play = item.querySelector(selPlay)
+            if(!title)return
+            // Mark button adding a special classname
+            const playid = title.pathname.replaceAll("/","_")
+            play.classList.add(playid)
+            return {
+                name : title.firstChild.innerText,
+                play : "a."+playid,
+                scroll : "a."+playid
+            }
+        })
+        return tracks
+    }
+
+    // Scrape current search results
     function getCurrentSearch() {
         const currentSearch = {}
         // Scrape tracks
@@ -86,19 +196,31 @@ var injected_dzctl;
             return { title : link.firstChild.innerText, path : link.pathname }
         })
         // TODO scrape albums
+        const albumSelector = '#page_naboo_search > div.search-results > div:nth-child(4) > ul > li:nth-child(1) > div > div.heading-4 > a'
+
         // TODO scrape artists
         return currentSearch
     }
+
+    // Build full status object
     function getStatus() {
         const currentSong = tojo(dzPlayer.getCurrentSong());
         const trackList = tojo(dzPlayer.getTrackList());
         const currentSearch = getCurrentSearch();
+        const navi = getMainNavi();
+        const playlists = getPlaylists();
+        const tracks = getTracks()
         return {
+            navi,
             currentSong,
             trackList,
-            currentSearch
+            currentSearch,
+            playlists,
+            tracks
         }
     }
+
+    // Recalc status and dispatch it if changed
     function checkStatus() {
         var newStatus = getStatus();
         if (JSON.stringify(newStatus) != JSON.stringify(status)) {
@@ -107,6 +229,8 @@ var injected_dzctl;
             peerctl.update(status)
         }
     }
+
+    // Create server peer
     function init2() {
         peerctl = new PeerCtlServer()
         peerctl.on('ready', function (info) {
@@ -121,12 +245,14 @@ var injected_dzctl;
 
 
 
-
-
-
+    // Global state
+    // Div with connection params
     var div
+    // Scraped status
     var status
+    // Peer controller object
     var peerctl
+    // Status refresh timmer
     var statusInterval
 
     // Support re-invoking the bookmarklet by unloading if already loaded
@@ -142,15 +268,7 @@ var injected_dzctl;
         }
     }
 
-
-
-
-
-
-
-
-
-
+    // Injection logic
     function inject(src, callback) {
         const script = document.createElement('script')
         script.type = 'text/javascript'
@@ -169,7 +287,8 @@ var injected_dzctl;
         })
     }
 
-
+    // This function is invoked when the bookmarklet finished loading this script
+    // Here we inject all other required scripts and then start
     window.peerctl_injected = function (script_src) {
         injectScripts(script_src)
     }
